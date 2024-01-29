c50c8c7882e63dc51c574337552ec84c44b3119c
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-29 15:55:59 -0800

    Add functionality for choosing search parameter Co-authored-by: Teddy Peterschmidt <teddyXIV@users.noreply.github.com>

--- InventoryTracker/Controllers/GamesController.cs
+++ InventoryTracker/Controllers/GamesController.cs
@@ -2,9 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using InventoryTracker.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System;
 using System.Threading.Tasks;
-using System.Data.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace InventoryTracker.Controllers
@@ -18,54 +16,20 @@ namespace InventoryTracker.Controllers
       _db = db;
     }
 
-    // public ActionResult Index()
-    // {
-    //   List<Game> model = _db.Games.ToList();
-    //   return View(model);
-    // }
-
-
-
-    // public async Task<IActionResult> Index(string searchQuery)
-    // {
-    //   List<Game> model = _db.Games.ToList();
-    //   // IEnumerable<Game> searchResults = model.Where(game => game.GameName.ToLower().Contains(gameName.ToLower())).ToList<Game>();
-
-    //   model = model.Where(game => game.GameName.Contains(searchQuery)).ToList<Game>();
-
-    //   return View(await model.ToListAsync());
-
-
-    // }
-
-    public async Task<IActionResult> IndexSearchOld(string searchString)
-    {
-      if (_db == null)
-      {
-        return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
-      }
-
-      var games = _db.Games.AsQueryable();
-      // from m in _db.Games
-      //             select m;
-
-      if (!String.IsNullOrEmpty(searchString))
-      {
-        games = games.Where(s => s.GameName!.Contains(searchString));
-      }
-
-      return View(await games.ToListAsync());
-
-    }
-
-
-    private async Task<List<Game>> SearchMethod(string searchQuery)
+    private async Task<List<Game>> SearchMethod(string searchQuery, string propertyName)
     {
       IQueryable<Game> gamesList = _db.Set<Game>();
 
       if (searchQuery != null)
       {
-        return await gamesList?.Where(s => s.GameName.Contains(searchQuery)).ToListAsync();
+        switch (propertyName)
+        {
+          case "GameDescription":
+            return await gamesList?.Where(s => s.GameDescription.Contains(searchQuery)).ToListAsync();
+          default: 
+            return await gamesList?.Where(s => s.GameName.Contains(searchQuery)).ToListAsync();
+        }
+        // return await gamesList?.Where(s => s.GameName.Contains(searchQuery)).ToListAsync();
       }
       else
       {
@@ -73,31 +37,11 @@ namespace InventoryTracker.Controllers
       }
     }
 
-    public async Task<IActionResult> Index(string searchQuery)
+    public async Task<IActionResult> Index(string searchQuery, string propertyName)
     {
-      List<Game> resultsList = await SearchMethod(searchQuery);
+      List<Game> resultsList = await SearchMethod(searchQuery, propertyName);
       return View(resultsList);
     }
-
-    // [HttpPost]
-    //     public async Task<IActionResult> Search(GameSearchViewModel viewModel)
-    //     {
-    //         if (ModelState.IsValid)
-    //         {
-    //             var searchResults = await _context.Games
-    //                 .Where(g => g.Name.Contains(viewModel.SearchTerm))
-    //                 .ToListAsync();
-
-    //             return View("SearchResults", searchResults);
-    //         }
-
-    //         return View(viewModel);
-    //     }
-
-
-
-
-
 
     public ActionResult Create()
     {
@@ -119,37 +63,3 @@ namespace InventoryTracker.Controllers
     }
   }
 }
-
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
-
-//     [HttpGet("/categories/{categoryId}/items/new")]
-//     public ActionResult New(int categoryId)
-//     {
-//       Category category = Category.Find(categoryId);
-//       return View(category);
-//     }
-
-//     [HttpPost("/items/delete")]
-//     public ActionResult DeleteAll()
-//     {
-//       Item.ClearAll();
-//       return View();
-//     }
-
-//     [HttpGet("/categories/{categoryId}/items/{itemId}")]
-//     public ActionResult Show(int categoryId, int itemId)
-//     {
-//       Item item = Item.Find(itemId);
-//       Category category = Category.Find(categoryId);
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       model.Add("item", item);
-//       model.Add("category", category);
-//       return View(model);
-//     }
-//   }
-// }
-
-
